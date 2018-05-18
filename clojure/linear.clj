76a381fa5f51dced5a4cae3bf5a8866f9e3a7cf9
fr0streaper <andrei1999kashukov@gmail.com>
2018-05-18 16:27:40 +0300

    Add post-conditions for linear.clj

--- clojure/linear.clj
+++ clojure/linear.clj
@@ -9,28 +9,32 @@
   (apply = (mapv count vs)))
 
 (defn checkMatrixes [ms]
-  (and (apply = (conj (mapv checkVectors ms) true))
-       (apply = (mapv count ms))
+  {:pre [(apply = (conj (mapv checkVectors ms) true))]}
+  (and (apply = (mapv count ms))
        (apply = (mapv (fn [x] (count (first x))) ms))))
 
 (defn checkMatrixesForMultiplication [ms]
-  (and (apply = (conj (mapv checkVectors ms) true))
-       (loop [ind 0 n (count ms) res true]
-         (if (= ind (dec n))
-           res
-           (recur (inc ind) n (and res (= (count (first (get ms ind)))
-                                          (count (get ms (inc ind))))))))))
+  {:pre [(apply = (conj (mapv checkVectors ms) true))]}
+  (loop [ind 0 n (count ms) res true]
+    (if (= ind (dec n))
+      res
+      (recur (inc ind) n (and res (= (count (first (get ms ind)))
+                                     (count (get ms (inc ind)))))))))
 
 (defn coordinateVectorApply [f & args]
-  { :pre [(checkVectors args) (> (count args) 0)] }
+  {:pre  [(checkVectors args) (> (count args) 0)]
+   :post [(vector? %) (number? (first %))]}
   (apply mapv f args))
 
 (defn coordinateMatrixApply [f & args]
-  { :pre [(checkMatrixes args) (> (count args) 0)] }
+  {:pre  [(checkMatrixes args) (> (count args) 0)]
+   :post [(vector? %) (vector? (first %)) (number? (first (first %)))
+          (= (count %) (count (first args))) (= (count (first %)) (count (first (first args))))]}
   (apply mapv f args))
 
 (defn getForm [x]
-  { :pre [] }
+  {:pre  [(not (string? x))]
+   :post [vector? %]}
   (loop [res [] t x]
     (if (number? t)
       res
@@ -39,7 +43,8 @@
 (def cmp (comparator (fn [x y] (> (count x) (count y)))))
 
 (defn canBroadcast [xf yf]
-  { :pre [] }
+  {:pre  [(vector? xf) (vector? yf)]
+   :post [(vector? xf) (vector? yf)]}
   (= xf (subvec yf (- (count yf) (count xf)))))
 
 (defn checkTensor
@@ -53,12 +58,14 @@
      :else (apply = (conj (mapv (fn [p] (checkTensor p (rest form))) x) (= (count x) (first form)) true)))))
 
 (defn checkTensors [args]
-  { :pre [(apply = (conj (mapv checkTensor args) true))] }
+  {:pre  [(apply = (conj (mapv checkTensor args) true))]
+   :post []}
   (let [mform (first (sort cmp (mapv getForm args)))]
     (apply = (conj (mapv (fn [x] (canBroadcast (getForm x) mform)) args) true))))
 
 (defn broadcast [x form]
-  { :pre [(canBroadcast (getForm x) form)] }
+  {:pre  [(canBroadcast (getForm x) form)]
+   :post []}
   (let [ext (vec (take (- (count form) (count (getForm x))) form))]
     (foldLeft
       x
@@ -67,7 +74,8 @@
       (vec (rseq ext)))))
 
 (defn coordinateTensorApply [f & args]
-  { :pre [(checkTensors args)] }
+  {:pre  [(checkTensors args)]
+   :post [(checkTensors (conj args %))]}
   (let [mform (first (sort cmp (mapv getForm args)))
         nargs (mapv (fn [x] (broadcast x mform)) args)]
     (cond
@@ -80,24 +88,23 @@
 ; --- vector functions
 
 (defn v+ [& args]
-  { :pre [(checkVectors args) (> (count args) 0)] }
   (apply coordinateVectorApply + args))
 
 (defn v- [& args]
-  { :pre [(checkVectors args) (> (count args) 0)] }
   (apply coordinateVectorApply - args))
 
 (defn v* [& args]
-  { :pre [(checkVectors args) (> (count args) 0)] }
   (apply coordinateVectorApply * args))
 
 (defn scalar [& args]
-  { :pre [(checkVectors args) (> (count args) 0)] }
+  {:pre  [(checkVectors args) (> (count args) 0)]
+   :post [(number? %)]}
   (apply + (apply v* args)))
 
 (defn vect [v & args]
-  { :pre [(apply = (conj (mapv count args) 3))
-          (checkVectors (conj args v))] }
+  {:pre  [(apply = (conj (mapv count args) 3))
+          (checkVectors (conj args v))]
+   :post [(vector? %) (number? (first %)) (= (count %) 3)]}
   (foldLeft
     v
     (fn [x y]
@@ -107,7 +114,8 @@
     args))
 
 (defn v*s [v & sc]
-  { :pre [] }
+  {:pre  [(vector? v) (number? (first v))]
+   :post [(vector? %) (number? (first %)) (checkVectors (vector v %))]}
   (foldLeft
     v
     (fn [x, s]
@@ -117,31 +125,34 @@
 ; --- matrix functions
 
 (defn transpose [m]
-  { :pre [(checkVectors m)] }
+  {:pre  [(checkVectors m)]
+   :post [(vector? %) (vector? (first %)) (number? (first (first %)))
+          (= (count %) (count (first m))) (= (count (first %)) (count m))]}
   (apply mapv vector m))
 
 (defn m+ [& args]
-  { :pre [(checkMatrixes args)] }
   (apply coordinateMatrixApply v+ args))
 
 (defn m- [& args]
-  { :pre [(checkMatrixes args)] }
   (apply coordinateMatrixApply v- args))
 
 (defn m* [& args]
-  { :pre [(checkMatrixes args)] }
   (apply coordinateMatrixApply v* args))
 
 (defn m*s [m & sc]
-  { :pre [(checkVectors m)] }
+  {:pre  [(checkVectors m)]
+   :post [(checkMatrixes (vector m %))]}
   (mapv (fn [v] (apply v*s v sc)) m))
 
 (defn m*v [m v]
-  { :pre [(checkVectors m) (= (count (first m)) (count v))] }
+  {:pre  [(checkVectors m) (= (count (first m)) (count v))]
+   :post [(= (count %) (count m))]}
   (mapv (fn [x] (scalar x v)) m))
 
 (defn m*m [& args]
-  { :pre [(checkMatrixesForMultiplication args)] }
+  {:pre  [(checkMatrixesForMultiplication args)]
+   :post [(vector? %) (vector? (first %)) (number? (first (first %)))
+          (= (count %) (count (first args))) (= (count (first %)) (count (first (last args))))]}
   (foldLeft
     (first args)
     (fn [x y]
@@ -154,13 +165,10 @@
 ; --- tensor functions
 
 (defn b+ [& args]
-  { :pre [(checkTensors args)] }
   (apply coordinateTensorApply + args))
 
 (defn b- [& args]
-  { :pre [(checkTensors args)] }
   (apply coordinateTensorApply - args))
 
 (defn b* [& args]
-  { :pre [(checkTensors args)] }
   (apply coordinateTensorApply * args))
